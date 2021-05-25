c5a30040e12b699e93b4a8e0033ba9f705e5bb5e
melniel <vitaminsprotien@gmail.com>
2021-05-25 16:25:54 +0800

    ---

--- application/views/admin/global/footer.php
+++ application/views/admin/global/footer.php
@@ -24,7 +24,7 @@
     <script src="<?=base_url()?>assets/admin/vendors/datatables.net-keytable/js/dataTables.keyTable.min.js"></script>
     <script src="<?=base_url()?>assets/admin/vendors/datatables.net-responsive/js/dataTables.responsive.min.js"></script>
     <script src="<?=base_url()?>assets/admin/vendors/datatables.net-responsive-bs/js/responsive.bootstrap.js"></script>
-    <script src="<?=base_url()?>assets/admin/vendors/datatables.net-scroller/js/datatables.scroller.min.js"></script>
+    <script src="<?=base_url()?>assets/admin/vendors/datatables.net-scroller/js/dataTables.scroller.min.js"></script>
     <script src="<?=base_url()?>assets/admin/vendors/jszip/dist/jszip.min.js"></script>
     <script src="<?=base_url()?>assets/admin/vendors/pdfmake/build/pdfmake.min.js"></script>
     <script src="<?=base_url()?>assets/admin/vendors/pdfmake/build/vfs_fonts.js"></script>
